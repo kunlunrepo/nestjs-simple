381e3bccce65386e539f00066c3d1f39bb00036b
kunlunrepo <482976531@qq.com>
2024-06-08 16:16:00 +0800

    新增接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('app')
+  getApp(): any {
+    return { code: 0, data: 'Hello Nestjs', msg: '请求成功' };
   }
 }
