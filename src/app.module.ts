543fd050570be932da4c9a4e1c88570a1df6679a
kunlunrepo <482976531@qq.com>
2024-06-08 19:35:14 +0800

    新增module中注入的解释

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { RangeModule } from './range/range.module';
 
+// 使用：Controller -> appService: AppService
+// 初始化1. imports 找到 module -> providers + exports -> AppService
+// 初始化2. providers -> AppService
+
 @Module({
   imports: [UserModule, RangeModule],
   controllers: [],
+  // 告诉nestjs -> 下面的class类放到DI中进行初始化
   providers: [],
+  // 告诉nestjs -> 下面的class需要在其他的地方用 别人导入module时可以使用下面的service
+  exports: [],
 })
 export class AppModule {}
